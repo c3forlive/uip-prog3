4dc8fd50867a4f4104c5426880cba3a691c3b926
c3forlive <karl550@hotmail.com>
2015-09-22 14:58:52 +0200

    Entrega del quiz

---
+++ Laboratorios/Quiz1.py
@@ -0,0 +1,20 @@
+
+#Quiz1
+#License by : Karl A. Hines
+#Calcular
+base = 5
+altura = 7
+area = base*altura
+perimetro = (2*base)+(2*altura)
+print('El area en centimetros es de '+str(area))
+print('El perimetro en centimetros es de '+str(perimetro))
+
+print('El area en metros es de '+str((area)/100))
+print('El perimetro en metros es de '+str((perimetro)/100))
+
+print('El area en pulgadas es de '+str((area)/2.54))
+print ('El perimetro en pulgadas es de '+str((perimetro)/2.54))
+
+
+
+
