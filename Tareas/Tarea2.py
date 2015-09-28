a9571569a5ede7f51b928a9d8116f036d973b7c7
c3forlive <karl550@hotmail.com>
2015-09-28 07:01:11 +0200

    entrega de la segunda tarea

---
+++ Tareas/Tarea2.py
@@ -0,0 +1,19 @@
+﻿#Tarea2
+#License by : Karl A. Hines
+#Velocidad
+#Crear un programa en Python que resuelva el siguiente problema 
+#de física: 
+#Una ambulancia se mueve con una velocidad de 120 km/h y 
+#necesita recorrer un tramo recto de 60km. 
+
+#Calcular el tiempo necesario, en segundos, 
+#para que la ambulancia llegue a su destino. 
+
+#La fórmula a utilizar es: velocidad = distancia / tiempo.
+
+
+velocidad = 120
+distancia = 60
+tiempo = velocidad / distancia
+print ('El tiempo a recorrer es '+str(tiempo))
+print ('El tiempo en segundos es '+str((tiempo)*60))
